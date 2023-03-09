ab03e9e41dab63643d5888659add6f6befff629c
daniondi-eth <113338845+daniondi-eth@users.noreply.github.com>
2023-03-09 11:20:41 +0100

    Update exportKnowledgeBase.js

--- exportKnowledgeBase.js
+++ exportKnowledgeBase.js
@@ -13,7 +13,27 @@ function exportKnowledgeBase(knowledgeBaseId) {
       statusParagraph.textContent = `Export job with ID ${jobId} created. Status: ${data.status}`;
       const responseDiv = document.getElementById('app');
       responseDiv.appendChild(statusParagraph);
-      checkExportJobStatus(jobId);
+      checkExportJobStatus(jobId)
+        .then((jobData) => {
+          // Download the JSON file
+          const downloadLink = document.createElement('a');
+          downloadLink.href = jobData.resultsUri;
+          downloadLink.download = `${knowledgeBaseId}.json`;
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          // Read the contents of the JSON file
+          const fileReader = new FileReader();
+          fileReader.onload = () => {
+            const json = fileReader.result;
+            console.log(json);
+            return (json);
+          };
+          fileReader.readAsText(jobData.resultsUri);
+        })
+        .catch((err) => {
+          console.log('There was a failure checking the export job status');
+          console.error(err);
+        });
     })
     .catch((err) => {
       console.log('There was a failure calling postKnowledgeKnowledgebaseLanguageBulkexport');
