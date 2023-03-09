7a3588da16b484b5ec3e40d7250e1d4591001139
daniondi-eth <113338845+daniondi-eth@users.noreply.github.com>
2023-03-09 09:40:42 +0100

    Update GetKnowledgeBase.js

--- GetKnowledgeBase.js
+++ GetKnowledgeBase.js
@@ -1,11 +1,10 @@
-function GetKnowledgeBase(exportCallback) {
-
+function GetKnowledgeBase(callback) {
   const platformClient = require('platformClient');
   let apiInstance = new platformClient.KnowledgeApi();
   let opts = {};
   // Get knowledge bases
   apiInstance.getKnowledgeKnowledgebases(opts)
-    .then((data) => {
+    .then((knowledgeBases) => {
         console.log(`Found ${knowledgeBases.entities.length} knowledge bases.`);
         const knowledgeBaseTable = document.getElementById('knowledge-bases-table').getElementsByTagName('tbody')[0];
         knowledgeBases.entities.forEach((kb) => {
@@ -15,126 +14,38 @@ function GetKnowledgeBase(exportCallback) {
             row.insertCell(2).appendChild(document.createTextNode(kb.description));
             row.insertCell(3).appendChild(document.createTextNode(kb.coreLanguage));
             row.insertCell(4).appendChild(document.createTextNode(kb.articleCount));
-            row.insertCell(5).innerHTML = '<button onclick="exportKnowledgeBase(\'' + kb.id + '\')">Export</button>';
+            row.insertCell(5).innerHTML = '<button onclick="exportKnowledgeBase(\'' + kb.id + '\')">Export Knowledge Base</button>';
+
         });
         if (callback) {
             callback();
         }
-        })
-        .catch((err) => {
-          console.log('There was a failure calling getKnowledgeKnowledgebases');
-          console.error(err);
-        });
-}	
+    })
+    .catch((err) => {
+        console.log('There was a failure calling getKnowledgeKnowledgebases');
+        console.error(err);
+    });
+}
 
-function KnowledgeExportJob(event, knowledgeBaseId) {
-  event.preventDefault(); // evita que la página se recargue
-
+function exportKnowledgeBase(knowledgeBaseId) {
   const platformClient = require('platformClient');
-  let apiInstance = new platformClient.KnowledgeApi();
-
-  const apiComponent = {
-
-    props: ['knowledgeBaseId'],
-
-    data: function() {
-      return {
-        response: null,
-        error: null,
-        exportJobId: null,
-        selectedKnowledgeBaseId: null
-      }
-    },
-
-    methods: {
-      createExportJob: function() {
-        this.response = null;
-        this.error = null;
-
-        apiInstance.postKnowledgeKnowledgebaseExportJobs(this.knowledgeBaseId, {})
-          .then((data) => {
-            console.log(`postKnowledgeKnowledgebaseExportJobs success! data: ${JSON.stringify(data, null, 2)}`);
-            this.exportJobId = data.id;
-            this.response = data;
-            this.waitForExportJob();
-          })
-          .catch((err) => {
-            console.log("There was a failure calling postKnowledgeKnowledgebaseExportJobs");
-            console.error(err);
-            this.error = err;
-          });
-      },
-
-      waitForExportJob: function() {
-        const checkJobStatus = () => {
-          apiInstance.getKnowledgeKnowledgebaseExportJobs(this.knowledgeBaseId, this.exportJobId)
-            .then((data) => {
-              console.log(`getKnowledgeKnowledgebaseExportJobs success! data: ${JSON.stringify(data, null, 2)}`);
-              if (data.status === 'completed') {
-                this.downloadExportFile(data.downloadUrl);
-              } else if (data.status === 'failed') {
-                this.error = new Error('Export job failed');
-              } else {
-                setTimeout(checkJobStatus, 5000);
-              }
-            })
-            .catch((err) => {
-              console.log("There was a failure calling getKnowledgeKnowledgebaseExportJobs");
-              console.error(err);
-              this.error = err;
-            });
-        };
-
-        setTimeout(checkJobStatus, 5000);
-      },
-
-      downloadExportFile: function(downloadUrl) {
-        axios.get(downloadUrl, {
-          responseType: 'arraybuffer'
-        })
-        .then((response) => {
-          console.log(`downloadExportFile success!`);
-
-          const fileName = `knowledge-base-${this.knowledgeBaseId}-export.json`;
-          const file = new Blob([response.data], { type: 'application/json' });
-          const link = document.createElement('a');
-          link.href = URL.createObjectURL(file);
-          link.download = fileName;
-          link.click();
-          this.response = `Export file downloaded as ${fileName}`;
-        })
-        .catch((err) => {
-          console.log("There was a failure downloading the export file");
-          console.error(err);
-          this.error = err;
-        });
-      }
-    },
-
-    template: '#api-component-template'
+  const apiInstance = new platformClient.KnowledgeApi();
+  const opts = {
+    'knowledgeBaseId': knowledgeBaseId,
+    'formatId': 'json'
   };
-
-  new Vue({
-    el: '#app',
-
-    components: {
-      'api-component': apiComponent
-    },
-
-    data: {
-      selectedKnowledgeBaseId: null
-    },
-
-    methods: {
-      setSelectedKnowledgeBaseId: function(knowledgeBaseId) {
-        this.selectedKnowledgeBaseId = knowledgeBaseId;
-      }
-    }
-  });
-
-  const instance = new Vue();
-
-  instance.$on('knowledge-base-selected', (knowledgeBaseId) => {
-    instance.setSelectedKnowledgeBaseId(knowledgeBaseId);
-  });
+  apiInstance.postKnowledgeKnowledgebaseLanguageBulkexport(opts)
+    .then((data) => {
+      console.log(data);
+      const jobId = data.jobId;
+      const statusParagraph = document.createElement('p');
+      statusParagraph.textContent = `Export job with ID ${jobId} created. Status: ${data.status}`;
+      const responseDiv = document.getElementById('app');
+      responseDiv.appendChild(statusParagraph);
+      checkExportJobStatus(jobId);
+    })
+    .catch((err) => {
+      console.log('There was a failure calling postKnowledgeKnowledgebaseLanguageBulkexport');
+      console.error(err);
+    });
 }
