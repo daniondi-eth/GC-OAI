eda57749216bb9d4f8848273e4e64a1eaeafc614
daniondi-eth <113338845+daniondi-eth@users.noreply.github.com>
2023-03-10 12:36:37 +0100

    Update exportKnowledgeBase.js

--- exportKnowledgeBase.js
+++ exportKnowledgeBase.js
@@ -1,37 +1,46 @@
 function exportKnowledgeBase() {
   const platformClient = require('platformClient');
   const apiInstance = new platformClient.KnowledgeApi();
-  
+
   const knowledgeBaseId = document.querySelector('input[name="knowledgeBaseRadio"]:checked').value;
   console.log("Exportando Knowledge Base con ID", knowledgeBaseId);
 
-  // Obtener lista de documentos
+  // Obtener documentos de la knowledge base
   apiInstance.getKnowledgeKnowledgebaseDocuments(knowledgeBaseId)
-    .then((documents) => {
-      console.log("Documentos descargados:", documents);
+    .then((response) => {
+      const documents = Array.isArray(response) ? response : [response]; // Aseguramos que response sea un array
+      const documentIds = documents.map(document => document.id);
+      console.log("IDs de documentos:", documentIds);
 
-      // Obtener variations para cada documento y construir array
-      const documentIds = documents.map(doc => doc.id);
-      const promises = documentIds.map(docId => apiInstance.getKnowledgeKnowledgebaseDocumentVariations(knowledgeBaseId, docId));
-      Promise.all(promises)
-        .then((variations) => {
-          console.log("Variaciones descargadas:", variations);
-
-          // Construir JSON resultado
-          const result = {
-            "knowledgeBase": {
-              "id": knowledgeBaseId
-            },
-            "documents": variations.flat() // unificar todas las variaciones de todos los documentos
+      // Obtener variaciones para cada documento
+      const documentPromises = documentIds.map(documentId =>
+        apiInstance.getKnowledgeKnowledgebaseDocumentVariations(knowledgeBaseId, documentId)
+      );
+      Promise.all(documentPromises)
+        .then((variationsResponses) => {
+          const documents = variationsResponses.map((variationsResponse, index) => {
+            return {
+              id: documentIds[index],
+              variations: variationsResponse
+            };
+          });
+          const knowledgeBase = {
+            id: knowledgeBaseId
           };
-          console.log("Resultado:", result);
+          const json = JSON.stringify({
+            knowledgeBase: knowledgeBase,
+            documents: documents
+          });
+          console.log("JSON descargado:", json);
+          return json;
         })
         .catch((error) => {
-          console.error("Error al obtener las variaciones:", error);
+          console.error('Error al obtener las variaciones de documentos:', error);
+          throw error;
         });
     })
     .catch((error) => {
-      console.error("Error al obtener los documentos:", error);
+      console.error('Error al obtener los documentos:', error);
+      throw error;
     });
 }
-
