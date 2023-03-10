63ff901d1cfb242e374a03bc96e0b14cad385e80
daniondi-eth <113338845+daniondi-eth@users.noreply.github.com>
2023-03-10 12:24:13 +0100

    Update exportKnowledgeBase.js

--- exportKnowledgeBase.js
+++ exportKnowledgeBase.js
@@ -5,51 +5,33 @@ function exportKnowledgeBase() {
   const knowledgeBaseId = document.querySelector('input[name="knowledgeBaseRadio"]:checked').value;
   console.log("Exportando Knowledge Base con ID", knowledgeBaseId);
 
-  // Crear el job de exportación
-  const opts = {
-    'exportFilter': {
-        'documentsFilter': null, 
-        'versionFilter': 'Latest'
-    }, 
-    'fileType': 'json'
-  };
-  const headers = {
-    'Origin': 'https://daniondi-eth.github.io/GC-OAI/' // para evitar error CORS
-  };
-  apiInstance.postKnowledgeKnowledgebaseExportJobs(knowledgeBaseId, opts, headers)
-    .then((response) => {
-      console.log(response);
-      const jobId = response.id;
+  // Obtener lista de documentos
+  apiInstance.getKnowledgeKnowledgebaseDocuments(knowledgeBaseId)
+    .then((documents) => {
+      console.log("Documentos descargados:", documents);
 
-      // Esperar a que el job termine
-      const intervalId = setInterval(() => {
-        apiInstance.getKnowledgeKnowledgebaseExportJob(knowledgeBaseId, jobId, headers)
-          .then((job) => {
-            console.log(job);
-            console.log("El status del job es: "+job.status);
-            if (job.status.toLowerCase() === 'completed') {
-              clearInterval(intervalId);
-              console.log("La URL de descarga es: "+job.downloadURL);
-              fetch(job.downloadURL, {
-                headers: headers // Agregar headers a la solicitud de descarga
-              })
-                .then(response => response.json())
-                .then(jsonData => {
-                  console.log("JSON descargado:", jsonData);
-                  // Aquí puedes hacer algo con el JSON descargado
-                })
-                .catch(error => {
-                  console.error("Error al descargar el JSON:", error);
-                  throw error;
-                });              
-            }
-          })
-          .catch((error) => {
-            console.error('Error al obtener el estado del job de exportación:', error);
-          });
-      }, 1000);
+      // Obtener variations para cada documento y construir array
+      const documentIds = documents.map(doc => doc.id);
+      const promises = documentIds.map(docId => apiInstance.getKnowledgeKnowledgebaseDocumentVariations(knowledgeBaseId, docId));
+      Promise.all(promises)
+        .then((variations) => {
+          console.log("Variaciones descargadas:", variations);
+
+          // Construir JSON resultado
+          const result = {
+            "knowledgeBase": {
+              "id": knowledgeBaseId
+            },
+            "documents": variations.flat() // unificar todas las variaciones de todos los documentos
+          };
+          console.log("Resultado:", result);
+        })
+        .catch((error) => {
+          console.error("Error al obtener las variaciones:", error);
+        });
     })
     .catch((error) => {
-      console.error('Error al crear el job de exportación:', error);
+      console.error("Error al obtener los documentos:", error);
     });
 }
+
